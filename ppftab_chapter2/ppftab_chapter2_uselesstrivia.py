d298b7df575729ba5305dc07233e06cf05beb194
cirby1 <calvin.irby01@gmail.com>
2023-02-13 06:44:22 -0600

    Completed Useless Trivia Program for Chapter 2. The formulas for the weight part are off for some reason?

--- ppftab_chapter2/ppftab_chapter2_uselesstrivia.py
+++ ppftab_chapter2/ppftab_chapter2_uselesstrivia.py
@@ -2,7 +2,7 @@
 
 name = input("Hi. What's your name? ")
 age = int(input("How old are you? "))
-weight = input("Okay. Last question. How many pounds do you weigh? ")
+weight = float(input("Okay. Last question. How many pounds do you weigh? "))
 print()
 
 print("If a poet ee cummings were to email you, he'd address you as " + name)
@@ -14,7 +14,18 @@ print("your name would become:")
 print(name.title()*5)
 print()
 
-print(age)
+#print(age)
 seconds = 0
 seconds = age * (31536000)
 print("You're over " + str(seconds) + " seconds old.")
+print()
+
+#Formula for calculating weight on the moon is: (Weight on Earth/Gravity on Earth) x Gravity on Moon. (165 Ibs/9.8 Earth Gravity) * 1.6 Moon Gravity.
+#print(weight)
+moonweight = (float(weight) / 9.8) * 1.6
+sunweight = (float(weight) / 9.8) * 27.9
+print("Did you know on the moon you would weigh only " + str(moonweight) + " pounds?")
+print("On the sun, you'd weigh " + str(sunweight) + " <but, ah... not for long>.")
+#print(moonweight)
+
+

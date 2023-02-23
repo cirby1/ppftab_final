181cfa1398a6316b3636fb370ac856f54a803bac
cirby1 <calvin.irby01@gmail.com>
2023-02-23 06:54:14 -0600

    -

---
+++ ppftab_chapter2/usingquoteswithinstrings.py
@@ -0,0 +1,9 @@
+#You can use either single or double quotes to create string values.
+
+#So 'Game Over' is the same as "Game Over"
+
+
+#This statement uses both kinds of strings.
+print ("Program 'Game Over' 2.0")
+
+
